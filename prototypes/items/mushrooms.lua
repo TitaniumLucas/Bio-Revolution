074bcc183510317e76bc8183353358e8cc66a591
TitaniumLucas <63296482+TitaniumLucas@users.noreply.github.com>
2024-07-12 15:33:58 +0200

    Add basic mushrooms as terrain generation test

---
+++ prototypes/items/mushrooms.lua
@@ -0,0 +1,13 @@
+local globals = require("globals")
+
+local basicMushroom = {
+	type = "item",
+	name = "br-basic-mushroom",
+	stack_size = globals.CONSTANTS.MUSHROOM_STACK_SIZE,
+	icon = globals.PLACEHOLDER.ITEM.ICON_FILEPATH,
+	icon_size = globals.PLACEHOLDER.ITEM.ICON_SIZE,
+	group = globals.PLACEHOLDER.ITEM.GROUP,
+	subgroup = globals.PLACEHOLDER.ITEM.SUBGROUP,
+}
+
+data:extend({ basicMushroom })
