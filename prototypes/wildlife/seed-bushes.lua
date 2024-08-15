09f903bb466411fb06a968072866f3d7b639bb01
TitaniumLucas <63296482+TitaniumLucas@users.noreply.github.com>
2024-08-16 00:49:46 +0200

    Merge seed bushes into single entity, and make it provide each currently implemented seed

--- prototypes/wildlife/seed-bushes.lua
+++ prototypes/wildlife/seed-bushes.lua
@@ -15,13 +15,21 @@ data:extend({
 		minable = {
 			mining_time = 1,
 			results = {
-				{ name = "br-corn-seed", amount_min = 1, amount_max = 5 },
+				{ type = "item", name = "br-corn-seed", probability = 0.5, amount = 1 },
+				{ type = "item", name = "br-wheat-seed", probability = 0.5, amount = 1 },
+				{ type = "item", name = "br-flax-seed", probability = 0.5, amount = 1 },
+				{ type = "item", name = "br-cotton-seed", probability = 0.5, amount = 1 },
 			},
 		},
 		render_layer = "object",
 		pictures = {
 			{
 				filename = globals.MODNAME .. "/graphics/entities/wildlife/seed-bush-01.png",
+				width = 64,
+				height = 64,
+			},
+			{
+				filename = globals.MODNAME .. "/graphics/entities/wildlife/seed-bush-02.png",
 				width = 64,
 				height = 64,
 			},
@@ -45,50 +53,50 @@ data:extend({
 			},
 		},
 	},
-	{
-		type = "noise-layer",
-		name = "br-seed-bush-02",
-	},
-	{
-		type = "simple-entity",
-		name = "br-seed-bush-02",
-		icon = globals.MODNAME .. "/graphics/entities/wildlife/seed-bush-02.png",
-		icon_size = 64,
-		flags = { "placeable-neutral" },
-		max_health = 50,
-		collision_box = { { -0.5, -0.5 }, { 0.5, 0.5 } },
-		selection_box = { { -1, -1 }, { 1, 1 } },
-		minable = {
-			mining_time = 1,
-			results = {
-				{ name = "br-wheat-seed", amount_min = 1, amount_max = 5 },
-			},
-		},
-		render_layer = "object",
-		pictures = {
-			{
-				filename = globals.MODNAME .. "/graphics/entities/wildlife/seed-bush-02.png",
-				width = 64,
-				height = 64,
-			},
-		},
-		map_color = { 1, 1, 1 }, --{ (10 / 255), (100 / 255), (10 / 255) },
-		autoplace = {
-			control = "br-seed-bush",
-			max_probability = 0.00051,
-			placement_density = 4,
-			sharpness = 0.95,
-			peaks = {
-				{
-					noise_layer = "br-seed-bush-02",
-					influence = 2.0,
-					noise_persistence = 0.2,
-					noise_octaves_difference = -2.5,
-					distance_optimal = 0,
-					distance_range = 5,
-					distance_max_range = 15,
-				},
-			},
-		},
-	},
+	-- {
+	-- 	type = "noise-layer",
+	-- 	name = "br-seed-bush-02",
+	-- },
+	-- {
+	-- 	type = "simple-entity",
+	-- 	name = "br-seed-bush-02",
+	-- 	icon = globals.MODNAME .. "/graphics/entities/wildlife/seed-bush-02.png",
+	-- 	icon_size = 64,
+	-- 	flags = { "placeable-neutral" },
+	-- 	max_health = 50,
+	-- 	collision_box = { { -0.5, -0.5 }, { 0.5, 0.5 } },
+	-- 	selection_box = { { -1, -1 }, { 1, 1 } },
+	-- 	minable = {
+	-- 		mining_time = 1,
+	-- 		results = {
+	-- 			{ name = "br-wheat-seed", amount_min = 1, amount_max = 5 },
+	-- 		},
+	-- 	},
+	-- 	render_layer = "object",
+	-- 	pictures = {
+	-- 		{
+	-- 			filename = globals.MODNAME .. "/graphics/entities/wildlife/seed-bush-02.png",
+	-- 			width = 64,
+	-- 			height = 64,
+	-- 		},
+	-- 	},
+	-- 	map_color = { 1, 1, 1 }, --{ (10 / 255), (100 / 255), (10 / 255) },
+	-- 	autoplace = {
+	-- 		control = "br-seed-bush",
+	-- 		max_probability = 0.00051,
+	-- 		placement_density = 4,
+	-- 		sharpness = 0.95,
+	-- 		peaks = {
+	-- 			{
+	-- 				noise_layer = "br-seed-bush-02",
+	-- 				influence = 2.0,
+	-- 				noise_persistence = 0.2,
+	-- 				noise_octaves_difference = -2.5,
+	-- 				distance_optimal = 0,
+	-- 				distance_range = 5,
+	-- 				distance_max_range = 15,
+	-- 			},
+	-- 		},
+	-- 	},
+	-- },
 })
