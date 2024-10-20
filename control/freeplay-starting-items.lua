901804a2c467986bf32dc9acfcd1cda5dde6f2f0
TitaniumLucas <63296482+TitaniumLucas@users.noreply.github.com>
2024-10-20 17:10:03 +0200

    Comment out debug code that prints to the console

--- control/freeplay-starting-items.lua
+++ control/freeplay-starting-items.lua
@@ -3,10 +3,10 @@ local respawn_items = {} -- Currently removes all items gained by respawning
 local wreckage_items = {} -- Currently removes all items in the crashed ship
 local scrap_items = {} -- Currently removes all items gained from destoying ship debris
 
-local testprint = "AAA"
+-- local testprint = "AAA"
 
 local replace_items = function()
-	testprint = "BBB"
+	-- testprint = "BBB"
 
 	remote.call("freeplay", "set_created_items", player_items)
 	remote.call("freeplay", "set_respawn_items", respawn_items)
@@ -14,17 +14,17 @@ local replace_items = function()
 	remote.call("freeplay", "set_debris_items", scrap_items)
 end
 
-local print_testprint = function(e)
-	if e.tick % 60 == 0 then
-		game.print(testprint)
-	end
-end
+-- local print_testprint = function(e)
+-- 	if e.tick % 60 == 0 then
+-- 		game.print(testprint)
+-- 	end
+-- end
 
 local scripts = {}
 
 scripts.on_init = replace_items
-scripts.events = {
-	[defines.events.on_tick] = print_testprint,
-}
+-- scripts.events = {
+-- 	[defines.events.on_tick] = print_testprint,
+-- }
 
 return scripts
